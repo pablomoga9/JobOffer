6e1e4d0c6bcb1c16f3a5a189a44d04cefde28cca
pablomoga9 <moyagarcia99@gmail.com>
2022-08-30 14:06:40 +0200

    add app

--- app.js
+++ app.js
@@ -6,4 +6,17 @@ require('./utils/dbMongo.js');
 const adRouter = require('./routes/adRoutes');
 
 //Middlewares
-const middle404 = require('./middlewares/error404.js')
+const middle404 = require('./middlewares/error404.js');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use('/api/', adRouter);
+
+app.use(middle404);
+
+app.listen(port,()=>{
+    console.log("Landed")
+})
