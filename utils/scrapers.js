ce65e9aa93f6166e3a5857aa80cd05c846b7fb4e
pablomoga9 <moyagarcia99@gmail.com>
2022-09-01 19:46:10 +0200

    scrap titles

--- utils/scrapers.js
+++ utils/scrapers.js
@@ -12,34 +12,35 @@ const extractAdsData = async(url,browser)=>{
 
         //Cada tarjeta tendrá imagen de empresa, título del puesto, nombre de la empresa, ciudad, presencial/remoto/híbrido, fecha de subida de la oferta, descripción, tipo de contrato, tipo de jornada y salario anual
         
-        // adData['img'] = await page.$eval('#app > div > div > div.ij-AdvertisingRoadblock > div.ij-AdvertisingRoadblockContainer > div.ij-AdvertisingRoadblockContainerChild > div > div > div.ij-ContentSearch-main > div.ij-ContentSearch-list > ul > li:nth-child(1) > div > div.sui-AtomCard-media > a > figure > div > div > div > figure > picture > img', img=>img.src);
-        
-        // adData['title'] = await page.$eval('#app > div > div > div.ij-AdvertisingRoadblock > div.ij-AdvertisingRoadblockContainer > div.ij-AdvertisingRoadblockContainerChild > div > div > div.ij-ContentSearch-main > div.ij-ContentSearch-list > ul > li:nth-child(1) > div > div.sui-AtomCard-info > div > h2 > a', title=>title.innerText);
-        
-        // adData['company'] = await page.$eval('#app > div > div > div.ij-AdvertisingRoadblock > div.ij-AdvertisingRoadblockContainer > div.ij-AdvertisingRoadblockContainerChild > div > div > div.ij-ContentSearch-main > div.ij-ContentSearch-list > ul > li:nth-child(1) > div > div.sui-AtomCard-info > div > h3 > a',company=>company.innerText);
-        
-        // adData['city'] = await page.$eval('#app > div > div > div.ij-AdvertisingRoadblock > div.ij-AdvertisingRoadblockContainer > div.ij-AdvertisingRoadblockContainerChild > div > div > div.ij-ContentSearch-main > div.ij-ContentSearch-list > ul > li:nth-child(1) > div > div.sui-AtomCard-info > div > ul:nth-child(6) > li:nth-child(1) > span', city=>city.innerText);
-        
-        // adData['attendance'] = await page.$eval('#app > div > div > div.ij-AdvertisingRoadblock > div.ij-AdvertisingRoadblockContainer > div.ij-AdvertisingRoadblockContainerChild > div > div > div.ij-ContentSearch-main > div.ij-ContentSearch-list > ul > li:nth-child(1) > div > div.sui-AtomCard-info > div > ul:nth-child(6) > li:nth-child(2)', attendance=>attendance.innerText);
-        
-        // adData['date'] = await page.$eval('#app > div > div > div.ij-AdvertisingRoadblock > div.ij-AdvertisingRoadblockContainer > div.ij-AdvertisingRoadblockContainerChild > div > div > div.ij-ContentSearch-main > div.ij-ContentSearch-list > ul > li:nth-child(1) > div > div.sui-AtomCard-info > div > ul:nth-child(6) > li:nth-child(3) > span', date=>date.innerText);
-        
-        // adData['description'] = await page.$eval('#app > div > div > div.ij-AdvertisingRoadblock > div.ij-AdvertisingRoadblockContainer > div.ij-AdvertisingRoadblockContainerChild > div > div > div.ij-ContentSearch-main > div.ij-ContentSearch-list > ul > li:nth-child(1) > div > div.sui-AtomCard-info > div > p',description=>description.innerText);
-        
-        // adData['contract'] = await page.$eval('#app > div > div > div.ij-AdvertisingRoadblock > div.ij-AdvertisingRoadblockContainer > div.ij-AdvertisingRoadblockContainerChild > div > div > div.ij-ContentSearch-main > div.ij-ContentSearch-list > ul > li:nth-child(1) > div > div.sui-AtomCard-info > div > ul:nth-child(8) > li:nth-child(1)',contract=>contract.innerText);
-        
-        // adData['hours'] = await page.$eval('#app > div > div > div.ij-AdvertisingRoadblock > div.ij-AdvertisingRoadblockContainer > div.ij-AdvertisingRoadblockContainerChild > div > div > div.ij-ContentSearch-main > div.ij-ContentSearch-list > ul > li:nth-child(1) > div > div.sui-AtomCard-info > div > ul:nth-child(8) > li:nth-child(2)', hours=>hours.innerText);
-        
         // adData['wage'] = await page.$eval('#app > div > div > div.ij-AdvertisingRoadblock > div.ij-AdvertisingRoadblockContainer > div.ij-AdvertisingRoadblockContainerChild > div > div > div.ij-ContentSearch-main > div.ij-ContentSearch-list > ul > li:nth-child(1) > div > div.sui-AtomCard-info > div > ul:nth-child(8) > li:nth-child(2)',wage=>wage.innerText);
         console.log("ok")
-
-        adData['card'] = await page.$$('#jobTitle-179cb34dbbaea635');
-        console.log(adData[0].innerHTML);
-        // for(let i = 0; i < adData.length; i++){
-        //     const element = adData[i];
-        //     console.log(element);
-        // }
+        const titles = await page.$$('td.resultContent > div > h2 > a > span');
+        const companyName = await page.$$('span.companyName > a')
+        console.log(titles[1]);
+        for(let i = 0; i< titles.length;i++){
+            const element = titles[i];
+            const titlesText = await page.evaluate(element=>element.textContent, element);
+            console.log(titlesText);
+        }
         
+        // const data = awaitpage.evaluate(()=>{
+        //     console.log("okok")
+        //     const title = document.querySelector('td.resultContent > div > h2 > a > span').innerHTML;
+        //     const companyName = document.querySelector('span.companyName > a').innerHTML;
+
+        //     return {
+        //         title,
+        //         companyName,
+        //         try1:console.log("okokok")
+        //     }
+        // });
+        
+        // adData['card'] = await page.$$('.job_seen_beacon');
+        console.log(data);
+    //    adData.map((element)=>{
+           
+    //    })
+
         return adData;
 
     }
