d90f2dbac6c329a80f35dc3748c31b0b1a5565c2
pablomoga9 <moyagarcia99@gmail.com>
2022-09-09 01:51:16 +0200

    jwt

---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+const routes = require('express').Router();
+const userController = require('../controllers/userControllers')
+
+routes.post('/login', userController.loginUser)
+routes.post('/register',userController.registerUser);
+routes.get('/logout',userController.logoutUser);
+routes.get('/recoverPassword7/:email', userController.recoverPassword);
+routes.put('/resetPassword/:recoverToken', userController.resetPassword);
+
+module.exports  = routes;
