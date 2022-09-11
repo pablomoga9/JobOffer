ecafc162a05f64da0da54952f5586f377abe69fe
Sofía Guardia Guillan <sofiamguardia@gmail.com>
2022-09-11 19:59:39 +0200

    add/ correction of logout route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const userController = require('../controllers/userControllers');
 
 routes.post('/login', userController.loginUser)
 routes.post('/signup',userController.registerUser);
-routes.get('/logout',userController.logout);
+routes.get('/logout/:email',userController.logout);
 routes.get('/recoverPassword/:email', userController.recoverPassword);
 routes.put('/resetPassword/:recoverToken', userController.restorePassword);
 
--- utils/dbElephant.js
+++ utils/dbElephant.js
@@ -1,12 +1,12 @@
 
-// const pg = require('pg');
-// require('dotenv').config()
-// const ClientClass = pg.Client
-// const pgUrl = `postgres://${process.env.ELEPH_USER}:${process.env.ELEPH_PASSWORD}@tyke.db.elephantsql.com/yamrybsr`
-// const client = new ClientClass(pgUrl)
-// clientConnected=client.connect()
-// console.log("Estas conectada a elephant", clientConnected);
+const pg = require('pg');
+require('dotenv').config()
+const ClientClass = pg.Client
+const pgUrl = `postgres://${process.env.ELEPH_USER}:${process.env.ELEPH_PASSWORD}@tyke.db.elephantsql.com/yamrybsr`
+const client = new ClientClass(pgUrl)
+clientConnected=client.connect()
+console.log("Estas conectada a elephant", clientConnected);
 
   
 
-//  module.exports=client
+ module.exports=client
